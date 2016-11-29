9c69dd56d5e08c1c03ab563cbd5936a83b180928
mavisli1022 <mavis.li9610@gmail.com>
2016-11-29 16:27:02 -0500

    add csv reading labels

--- grayscale.py
+++ grayscale.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy
 import os
+import csv
 from scipy.misc import imread
 from scipy.misc import imresize
 from scipy.spatial import distance
@@ -11,13 +12,31 @@ from PIL import Image
 
 
 
+def loadcsv():
+    filename = 'train.csv'
+    labels = np.zeros(7000)
+
+    with open(filename, 'rb') as f:
+        reader = csv.reader(f)
+        i = 0
+        firstline = True
+        for row in reader:
+            if firstline:
+                firstline = False
+                continue
+            else:
+                labels[i] = int(row[1])
+                print labels[i]
+                i = i+1
+
+
 def rgb2gray(rgb):
     r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
     grey = 0.2989*r + 0.5870*g + 0.1140*b
     return grey/255
 
 
-def process_img(in_path, out_path):
+def i(in_path, out_path):
     if not os.path.exists(in_path):
         return 'Input path does not exists, please enter a valid path'
     if os.path.exists(out_path):
@@ -27,4 +46,8 @@ def process_img(in_path, out_path):
         for f in listdir(in_path):
             if isfile(join(in_path, f)):
                 image = rgb2gray(scipy.misc.imread(join(in_path, f)))
+                print np.array(image)
                 imsave(join(out_path, f), image)
+
+if __name__ == '__main__':
+
